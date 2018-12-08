bab6a2ecd7f032944bddeaa14a4b53302e5adf03
William Eisert <weisert@eisertdev.com>
2018-12-08 02:40:42 -0800

    Point back to prod.

--- src/utils/configureApollo.js
+++ src/utils/configureApollo.js
@@ -3,8 +3,8 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 
-// const HOST = 'https://refsheet.net';
-const HOST = 'http://dev.refsheet.net:5000';
+const HOST = 'https://refsheet.net';
+// const HOST = 'http://dev.refsheet.net:5000';
 
 const httpLink = createHttpLink({
   uri: `${HOST}/graphql`,
